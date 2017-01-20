3b94dd28fab88093e747b5a63adae8084982b901
Antonin Bernardin <antonin.bernardin@gmail.com>
2017-01-20 23:46:10 +0100

    gull dead trigger and animation

--- Assets/Scripts/GullAI.cs
+++ Assets/Scripts/GullAI.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class GullAI : MonoBehaviour
 {
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
 	void Update()
     {
         float offsetX = 0f;//Mathf.Cos(Time.time);
@@ -12,15 +19,29 @@ public class GullAI : MonoBehaviour
             transform.position.x + offsetX,
             transform.position.y,
             transform.position.z
-        ); ;
+        );
 	}
     
     void OnCollisionEnter2D(Collision2D other)
     {
         GameObject go = other.gameObject;
+
         if (go.tag == "Rubbish")
         {
-            Destroy(gameObject);
+            bool isDead = anim.GetBool("isDead");
+            if(!isDead)
+            {
+                anim.SetBool("isDead", true);
+
+                Vector2 collisionPoint = other.contacts[0].point;
+                Vector2 gullPosition = new Vector2(this.transform.position.x, this.transform.position.y);
+
+                Rigidbody2D rb = gameObject.AddComponent<Rigidbody2D>();
+                Vector2 dir = -Vector3.Normalize(collisionPoint - gullPosition);
+                rb.AddForce(new Vector2(dir.x, dir.y) * 500f);
+            }
+
+
         }
     }
 }
