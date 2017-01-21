6d97e37589204d46c0195badd54997e6ccbfbcb5
Antonin Bernardin <antonin.bernardin@gmail.com>
2017-01-21 05:10:33 +0100

    rubbish dispenser

---
+++ Assets/Scripts/Controllers_AI/RubbishController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RubbishController : MonoBehaviour
+{
+    public float timeToLive = 5f;
+
+    private IEnumerator coroutineKillMe;
+    
+    void Start()
+    {
+        coroutineKillMe = KillMe();
+        StartCoroutine(coroutineKillMe);
+    }
+
+    private IEnumerator KillMe()
+    {
+        yield return new WaitForSeconds(timeToLive);
+        DestroyImmediate(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Mediator/RubbishDispenser.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class RubbishDispenser : MonoBehaviour
+{
+    public float tempo = 2f;
+    public RubbishController rubbishPrefab;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnRubbish", 0f, tempo);
+    }
+
+    private void SpawnRubbish()
+    {
+        GameObject.Instantiate<RubbishController>(rubbishPrefab, transform.position, transform.rotation);
+    }
+}
