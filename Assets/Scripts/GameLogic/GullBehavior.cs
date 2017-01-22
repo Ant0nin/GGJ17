bdabe1377df82bc8eb2f23197104ab1f2035fcbd
Antonin Bernardin <antonin.bernardin@gmail.com>
2017-01-22 11:22:33 +0100

    blood explosion when we kill a gull

--- Assets/Scripts/GameLogic/GullBehavior.cs
+++ Assets/Scripts/GameLogic/GullBehavior.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class GullBehavior : MonoBehaviour
 {
+    public float bloodEmissionTimeout = 2f;
+
     private Animator anim;
+    private ParticleSystem bloodEmitter;
 
     void Start()
     {
         anim = GetComponent<Animator>();
+        bloodEmitter = GetComponent<ParticleSystem>();
     }
     
-    void OnCollisionEnter2D(Collision2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         GameObject go = other.gameObject;
 
@@ -22,16 +26,31 @@ public class GullBehavior : MonoBehaviour
             {
                 anim.SetBool("isDead", true);
 
-                Vector2 collisionPoint = other.contacts[0].point;
+                /*Vector2 collisionPoint = other.contacts[0].point;
                 Vector2 gullPosition = new Vector2(this.transform.position.x, this.transform.position.y);
-
                 Rigidbody2D rb = gameObject.AddComponent<Rigidbody2D>();
                 Vector2 dir = -Vector3.Normalize(collisionPoint - gullPosition);
-                rb.AddForce(new Vector2(dir.x, dir.y) * 500f); // TODO : ajust
+                rb.AddForce(new Vector2(dir.x, dir.y) * 500f);*/
 
+                bloodEmitter.Play();
                 Destroy(GetComponent<CreatureMove>());
+                Destroy(GetComponent<SpriteRenderer>());
+                Destroy(GetComponent<BoxCollider2D>());
+                StartCoroutine(StopBloodEmission());
             }
-            
         }
+    }
+
+    private IEnumerator StopBloodEmission()
+    {
+        yield return new WaitForSeconds(bloodEmissionTimeout);
+        bloodEmitter.enableEmission = false;
+        StartCoroutine(AutoDestroy());
+    }
+
+    private IEnumerator AutoDestroy()
+    {
+        yield return new WaitForSeconds(2.5f);
+        Destroy(this.gameObject);
     }
 }
