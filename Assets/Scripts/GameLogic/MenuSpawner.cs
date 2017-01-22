f04f72144c321d07f1d0dbbfc30e9a336edf1922
Antonin Bernardin <antonin.bernardin@gmail.com>
2017-01-22 13:58:09 +0100

    add menu to play again

---
+++ Assets/Scripts/GameLogic/MenuSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSpawner : MonoBehaviour {
+
+    private GameObject targetCanvasGameobj;
+    private int countGulls;
+    private bool hasWon = false;
+
+    void Start()
+    {
+        targetCanvasGameobj = GameObject.Find("UI_Menu");
+    }
+
+	void Update () {
+
+        if(!hasWon)
+        {
+            countGulls = GameObject.FindGameObjectsWithTag("Gull").Length;
+            if (countGulls == 0)
+                hasWon = true;
+        }
+
+        if(hasWon)
+        {
+            targetCanvasGameobj.GetComponent<Canvas>().enabled = true;
+            DestroyImmediate(this.gameObject);
+        }
+	}
+}
---
+++ Assets/Scripts/GameLogic/SceneReloader.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneReloader : MonoBehaviour {
+	
+	public void ReloadScene()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+}
